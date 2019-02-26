70259a5046663a0083926954975b76e597d8039c
Sander van der Linden <toxaris@gmail.com>
2019-02-26 15:55:07 +0100

    Start of ToxDialog conversion from VB.Net

---
+++ ToxDialog/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace ToxDialog
+{
+    internal static class Helper
+    {
+        /// <summary>
+        /// Gets the name of the application.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public static string ApplicationName()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            var applicationTitle = ((AssemblyTitleAttribute)entryAssembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title;
+            if (string.IsNullOrWhiteSpace(applicationTitle))
+            {
+                applicationTitle = entryAssembly.GetName().Name;
+            }
+            return applicationTitle;
+        }
+    }
+}
---
+++ ToxDialog/ToxDialog.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Forms;
+
+namespace ToxDialog
+{
+    /// <summary>
+    /// Displays a Vista-like message box or task dialog that can contain text, buttons, symbols and other elements that inform and instruct the user.
+    /// </summary>
+    public sealed class ToxDialog
+    {
+        #region Messagebox
+
+        private static readonly IWin32Window NullWindow = null;
+
+        /// <summary>
+        /// Displays a Vista-like message box with specified text.
+        /// </summary>
+        /// <param name="text">The text to display in the Vista-like message box.</param>
+        /// <returns>One of the DialogResult values.</returns>
+        public static DialogResult Show(string text)
+        {
+            return Show(NullWindow, text);
+        }
+
+        /// <summary>
+        /// Displays a Vista-like message box in front of the specified object and with the specified text.
+        /// </summary>
+        /// <param name="owner">An implementation of IWin32Window that will own the modal dialog box.</param>
+        /// <param name="text">The text to display in the Vista-like message box.</param>
+        /// <returns>One of the DialogResult values.</returns>
+        public static DialogResult Show(IWin32Window owner, string text)
+        {
+            return Show(owner, text, Helper.ApplicationName());
+        }
+
+        /// <summary>
+        /// Displays a Vista-like message box with specified text and caption.
+        /// </summary>
+        /// <param name="text">The text to display in the Vista-like message box.</param>
+        /// <param name="caption">The text to display in the title bar of the Vista-like message box.</param>
+        /// <returns>One of the DialogResult values.</returns>
+        public static DialogResult Show(string text, string caption)
+        {
+            return Show(NullWindow, text, caption);
+        }
+
+        #endregion Messagebox
+    }
+}
